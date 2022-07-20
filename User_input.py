ba0551bc13523162584a0d052f773e625ffb51ad
okatoue <101699984+okatoue@users.noreply.github.com>
2022-07-20 05:21:04 -0700

    Got rid of the option to sort by photos and videos

--- User_input.py
+++ User_input.py
@@ -20,7 +20,6 @@ year_yn = Input_functions.year_input()
 
 month_yn  = False
 day_yn    = False
-pv_folder = False
 m_y_input = False
 
 # If user said yes, it then asks them if they want to sort files by months
@@ -35,10 +34,7 @@ if year_yn:
 else:
     m_y_input = Input_functions.month_year_input()
 
-# Asks user if they want there files separated into two category, photos and videos
-pv_folder = Input_functions.pv_input()
+fr_input = Input_functions.folder_remover_input()
 
-# List of what the user has inputed
-qa_input = (year_yn, month_yn, day_yn, pv_folder, m_y_input)
-
-print(qa_input)
+# List of what the user has inputted
+qa_input = (year_yn, month_yn, day_yn, m_y_input)
